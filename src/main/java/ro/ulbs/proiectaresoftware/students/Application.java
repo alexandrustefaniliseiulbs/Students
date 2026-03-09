64ccbbf07b83f08bc38577e4ad72d4d2261859f3
alexandrustefaniliseiulbs <alexandrustefan.ilisei@ulbsibiu.ro>
2026-03-09 11:06:55 +0200

    v2

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,4 +1,17 @@
 package ro.ulbs.proiectaresoftware.students;
 
 public class Application {
+    public static void main(String[] args) {
+        Student s1 = new Student(112, "Ioan", "Popa", "TI21/1");
+        Student s2 = new Student(112, "Maria", "Oprea", "TI21/1");
+        Student s3 = new Student(120, "Alis", "Popa", "TI21/2");
+        Student s4 = new Student(122, "Mihai", "Vecerdea", "TI22/1");
+        Student s5 = new Student(122, "Eugen", "Uritescu", "TI22/2");
+
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
+        System.out.println(s3.toString());
+        System.out.println(s4.toString());
+        System.out.println(s5.toString());
+    }
 }
--- src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -1,4 +1,36 @@
 package ro.ulbs.proiectaresoftware.students;
 
 public class Student {
+    int numarMatricol;
+    String nume;
+    String prenume;
+    String formațieDeStudiu;
+
+    public Student(int numarMatricol, String nume, String prenume, String formațieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.formațieDeStudiu = formațieDeStudiu;
+    }
+
+    public int getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public String getFormațieDeStudiu() {
+        return formațieDeStudiu;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%05d %10s %10s %10s",numarMatricol,prenume,nume,formațieDeStudiu);
+    }
 }
